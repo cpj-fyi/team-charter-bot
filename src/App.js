fbea3a44dc845b8a2cdd1b392aa14e811e4b6813
cpj-fyi <clay@cpj.fyi>
2024-10-08 08:03:13 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Team Charter Bot</h1>
+      <p>Welcome to the Team Charter Bot!</p>
+    </div>
+  );
+}
+
+export default App;
